29c41b7066e9657adeb800dee8e86d317dd1cd3a
Mauro Casciati <maurocasciati@gmail.com>
2022-05-29 01:11:45 -0300

    ReactNavigation base configuration

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import DetalleRecorrido from './src/screens/DetalleRecorrido';
+import HomeScreen from './src/screens/Home';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Inicio" component={HomeScreen} />
+        <Stack.Screen name="Detalle Recorrido" component={DetalleRecorrido} />
+      </Stack.Navigator>
+      <StatusBar style='auto' />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
