a0f89822cc2fcd48095c1d2278ac57f1c3015d13
nespindola <nespindola@lightercapital.com>
2022-06-13 18:00:05 -0300

    agrego la libreria react-form-hook para hacer mas facil la creacion, manejo y validacion de formularios, agrego logica para autenticar al usuario y para crear usuarios, agrego componente custom para crear inputs de texto que utilicen react-form-hook, agrego clases estaticas para manejar mensajes del sistema desde un solo lugar, agrego clases estaticas para validaciones y expresiones regulares, agrego la libreria react-native-dotenv para poder tener variables de entorno como la url base del backend (esto nos facilita el trabajo para automatizar los deploy)

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,54 +1,102 @@
+import { REACT_APP_BASE_URL } from '@env';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 import React, { useState } from 'react';
-import { View, TextInput } from 'react-native';
+import { Controller, useForm } from 'react-hook-form';
+import { View, TextInput, Text, Button } from 'react-native';
+import { VALIDACIONES } from '../domain/Validaciones';
 import { styles } from '../styles/styles';
+import CustomTextInput from './form/CustomTextInput';
 import PrimaryButton from './PrimaryButton';
 
+type FormType = {
+  nombre: string,
+  apellido: string,
+  email: string,
+  contraseña: string
+};
+
 export default function SignUp() {
-  const [nombre, setNombre] = useState('');
-  const [apellido, setApellido] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
-  const onLogin = () => {
-    console.log({nombre});
-    console.log({apellido});
-    console.log({email});
-    console.log({password});
+  const [signUpError, setSignUpError] = useState('');
+  
+  const {control, handleSubmit, formState: {errors}} = useForm<FormType>({
+    defaultValues: {
+      nombre: '',
+      apellido: '',
+      email: '',
+      contraseña: ''
+    }
+  });
+
+  const onSubmit = async (data: FormType) => {
+    try{
+      const resp = await axios.post(`${REACT_APP_BASE_URL}/Usuario/registrar`, data);
+      if(resp){
+        alert(`El usuario ${data.email} fue registrado con exito`);
+      }
+    }
+    catch(error){
+      if(axios.isAxiosError(error)){
+        const err = error as AxiosError;
+        const errorResponse = err.response as AxiosResponse<{message?: string, title: string}>;
+        if(errorResponse.data)
+          setSignUpError(errorResponse.data.message ?? errorResponse.data.title);
+        else
+          setSignUpError(err.message);
+      }
+      else{
+        setSignUpError(error as string);
+      }
+    }
   };
-
+  
   return (
     <>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.textInput}
-          placeholder='Nombre'
-          onChangeText={setNombre}
-        />
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.textInput}
-          placeholder='Apellido'
-          onChangeText={setApellido}
-        />
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.textInput}
-          placeholder='Email'
-          onChangeText={setEmail}
-        />
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.textInput}
-          placeholder='Contraseña'
-          secureTextEntry={true}
-          onChangeText={setPassword}
-        />
-      </View>
-    
-      <PrimaryButton name='REGISTRARSE' action={onLogin} />
+      <CustomTextInput
+        control={control}
+        name="nombre"
+        errors={errors}
+        placeholder="Nombre"
+        rules={VALIDACIONES.TEXTO_NO_VACIO}
+      />
+
+      <CustomTextInput
+        control={control}
+        name="apellido"
+        errors={errors}
+        placeholder="Apellido"
+        rules={VALIDACIONES.TEXTO_NO_VACIO}
+      />
+
+      <CustomTextInput
+        control={control}
+        name="email"
+        errors={errors}
+        placeholder="E-Mail"
+        rules={VALIDACIONES.EMAIL}
+      />
+
+      <CustomTextInput
+        control={control}
+        name="contraseña"
+        errors={errors}
+        placeholder="Contraseña"
+        rules={VALIDACIONES.TEXTO_NO_VACIO}
+        ocultarTexto={true}
+      />
+
+      {signUpError ? ErrorText(signUpError) : null}
+      <PrimaryButton name="Submit" action={handleSubmit(onSubmit)} />
     </>
+  );
+}
+
+function ErrorText(mensajeError : string){
+  return (
+    <View>
+      <Text>
+        {`Error: ${mensajeError}`}
+      </Text>
+    </View>
   );
 }
