c96e10d30691e02736ca47408107059c57bb6916
Mauro Casciati <maurocasciati@gmail.com>
2022-07-09 20:19:44 -0300

    Fix ver tutores nuevos

--- src/screens/RecorridoDetalle.tsx
+++ src/screens/RecorridoDetalle.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useContext, useState } from 'react';
 import * as Location from 'expo-location';
 import { View, Text, StyleSheet } from 'react-native';
 import { RecorridoDetalleProps } from '../components/Navigation';
@@ -8,69 +8,94 @@ import SecondaryButton from '../components/SecondaryButton';
 import MapViewRecorrido from '../components/MapViewRecorrido';
 import { mapDateTimeStringToTime } from '../utils/date.utils';
 import ModalConfirmacion from '../components/ModalConfirmacion';
+import { Recorrido } from '../domain/Recorrido';
+import { AuthContext } from '../auth/AuthProvider';
+import { getRecorrido } from '../services/recorrido.service';
+import ErrorText from '../components/ErrorText';
+import { useFocusEffect } from '@react-navigation/native';
 
 export default function RecorridoDetalle({ route, navigation }: RecorridoDetalleProps) {
   const { recorrido } = route.params;
 
-  useEffect(() => {
-    (async () => {
-      await Location.requestForegroundPermissionsAsync();
-    })();
-  }, []);
+  const [recorridoFetched, setRecorridoFetched] = useState<Recorrido | undefined>(undefined);
+  const [mensajeError, setMensajeError] = useState<string | null>(null);
+  
+  const { token } = useContext(AuthContext);
 
-  const tieneEscuela = !!recorrido.escuela;
-  const tienePasajeros = !!recorrido.pasajeros && recorrido.pasajeros.length > 0;
+  useFocusEffect(
+    useCallback(() => {
+      let componentIsFocused = true;
+
+      (async () => {
+        await Location.requestForegroundPermissionsAsync();
+        try {
+          const recorridoFetchedResponse = await getRecorrido(token, recorrido.id);
+          if (componentIsFocused && recorridoFetchedResponse) {
+            setRecorridoFetched(recorridoFetchedResponse);
+          }
+        } catch(error) {
+          setMensajeError(error as string);
+        }
+      })();
+
+      return () => { componentIsFocused = false; };
+    }, [])
+  );
+
+  const tieneEscuela = !!recorridoFetched?.escuela;
+  const tienePasajeros = !!recorridoFetched?.pasajeros && recorridoFetched.pasajeros.length > 0;
 
   return (
     <View style={styles.container}>
       <View>
-        <MapViewRecorrido recorrido={recorrido} />
+        <MapViewRecorrido recorrido={recorridoFetched || recorrido} />
       </View>
 
       { tieneEscuela && tienePasajeros &&
       <View style={localstyles.detailsContainer}>
-        <View style={localstyles.recorridoContainer}>
+        <View style={localstyles.recorridoFetchedContainer}>
           <View style={{ flex: 1 }}>
-            <Text style={localstyles.title}>{ recorrido.nombre }</Text>
+            <Text style={localstyles.title}>{ recorridoFetched.nombre }</Text>
           </View>
           <View style={{ paddingHorizontal: 20, alignContent: 'center' }}>
-            <Text style={localstyles.type}>{recorrido.esRecorridoDeIda ? 'Ida' : 'Vuelta'}</Text>
-            <Text style={localstyles.hour}>{mapDateTimeStringToTime(recorrido.horario)}</Text>
+            <Text style={localstyles.type}>{recorridoFetched.esRecorridoDeIda ? 'Ida' : 'Vuelta'}</Text>
+            <Text style={localstyles.hour}>{mapDateTimeStringToTime(recorridoFetched.horario)}</Text>
           </View>
           <View style={localstyles.columns}>
             <View style={localstyles.botonContainer}>
-              <SecondaryButton name='Editar' action={() => navigation.navigate('RecorridoEdicion', { recorrido } )}></SecondaryButton>
+              <SecondaryButton name='Editar' action={() => navigation.navigate('RecorridoEdicion', { recorrido: recorridoFetched } )}></SecondaryButton>
             </View>
           </View>
         </View>
         <View style={localstyles.escuelaContainer}>
           <View style={{ flex: 1 }}>
-            <Text style={localstyles.title}>{ recorrido.escuela?.nombre }</Text>
-            <Text style={localstyles.subtitle}>{ recorrido.escuela?.direccion.domicilio }</Text>
+            <Text style={localstyles.title}>{ recorridoFetched.escuela?.nombre }</Text>
+            <Text style={localstyles.subtitle}>{ recorridoFetched.escuela?.direccion.domicilio }</Text>
           </View>
           <View style={localstyles.columns}>
             <View style={localstyles.botonContainer}>
-              <SecondaryButton name='Ver escuela' action={() => navigation.navigate('EscuelaEdicion', { recorrido, dataRecorrido: null, escuela: recorrido.escuela } )}></SecondaryButton>
+              <SecondaryButton name='Ver escuela' action={() => navigation.navigate('EscuelaEdicion', { recorrido: recorridoFetched, dataRecorrido: null, escuela: recorridoFetched.escuela } )}></SecondaryButton>
             </View>
           </View>
         </View>
         <View style={localstyles.pasajerosContainer}>
-          <Text style={localstyles.pasajerosText}>Cantidad de pasajeros: {recorrido.pasajeros.length}</Text>
+          <Text style={localstyles.pasajerosText}>Cantidad de pasajeros: {recorridoFetched.pasajeros.length}</Text>
           <View style={localstyles.botonContainer}>
-            <SecondaryButton name='Ver listado' action={() => navigation.navigate('PasajeroListado', { recorrido })}></SecondaryButton>
+            <SecondaryButton name='Ver listado' action={() => navigation.navigate('PasajeroListado', { recorrido: recorridoFetched })}></SecondaryButton>
           </View>
         </View>
         <View style={localstyles.footerButton}>
-          <PrimaryButton name={'INICIAR'} action={() => navigation.navigate('RecorridoEnCurso', { recorrido })}/>
+          { mensajeError && ErrorText(mensajeError) }
+          <PrimaryButton name={'INICIAR'} action={() => navigation.navigate('RecorridoEnCurso', { recorrido: recorridoFetched })}/>
         </View>
       </View>
       }
 
       <ModalConfirmacion
         visible={!tieneEscuela || !tienePasajeros}
-        text={`El recorrido ${recorrido.nombre} está incompleto. ¿Desea editarlo para completarlo ahora?`}
+        text={`El recorrido ${recorridoFetched?.nombre} está incompleto. ¿Desea editarlo para completarlo ahora?`}
         cancel={() => navigation.navigate('RecorridoListado')}
-        confirm={() => navigation.navigate('RecorridoEdicion', { recorrido } )}
+        confirm={() => navigation.navigate('RecorridoEdicion', { recorrido: recorridoFetched || recorrido } )}
       />
     </View>
   );
@@ -85,7 +110,7 @@ const localstyles = StyleSheet.create({
     margin: 25,
     flex: 1,
   },
-  recorridoContainer: {
+  recorridoFetchedContainer: {
     flexDirection: 'row',
     padding: 10,
     paddingBottom: 10,
