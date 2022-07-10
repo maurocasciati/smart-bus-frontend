6c80df88b20cf3ee63391f17102bd3637282a172
Mauro Casciati <maurocasciati@gmail.com>
2022-07-10 18:28:07 -0300

    Agrego id usuario al auth context

--- src/auth/AuthProvider.tsx
+++ src/auth/AuthProvider.tsx
@@ -6,6 +6,8 @@ export type AuthContextType = {
   saveToken: (token: string) => void;
   rol: RolUsuario | null;
   saveRol: (idTipoDeUsuario: number) => void;
+  id: number | null;
+  saveId: (id: number) => void;
 }
 
 export const AuthContext = createContext<AuthContextType>({} as AuthContextType);
@@ -13,12 +15,14 @@ export const AuthContext = createContext<AuthContextType>({} as AuthContextType)
 export const AuthProvider: React.FC<React.ReactNode> = ({ children }) => {
   const [token, setToken] = useState<string | null>(null);
   const [rol, setRol] = useState<RolUsuario | null>(null);
+  const [id, setId] = useState<number | null>(null);
 
   const saveToken = (token: string) => setToken(token);
   const saveRol = (idTipoDeUsuario: number) => setRol(idTipoDeUsuario as RolUsuario);
+  const saveId = (id: number) => setId(id);
 
   return (
-    <AuthContext.Provider value={{token, saveToken, rol, saveRol}}>
+    <AuthContext.Provider value={{token, saveToken, rol, saveRol, id, saveId}}>
       {children}
     </AuthContext.Provider>
   );
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -11,7 +11,7 @@ export default function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loginError, setLoginError] = useState<string | null>(null);
-  const { saveToken, saveRol } = useContext(AuthContext);
+  const { saveToken, saveRol, saveId } = useContext(AuthContext);
 
   const onLogin = async (e: GestureResponderEvent) => {
     e.preventDefault();
@@ -22,6 +22,7 @@ export default function SignIn() {
       const resp = await signIn(email, password);
       resp?.data.token ? saveToken(resp.data.token) : setLoginError('Usuario no autorizado');
       resp?.data.idTipoDeUsuario ? saveRol(resp.data.idTipoDeUsuario) : setLoginError('Tipo de usuario inválido');
+      resp?.data.id ? saveId(resp.data.id) : setLoginError('Usuario no registrado');
     } catch(error) {
       setLoginError(error as string);
     }
